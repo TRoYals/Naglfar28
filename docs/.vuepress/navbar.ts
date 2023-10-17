304e2406e953f1ad0c94376b59fcb9ca835c5b72
TRoYals <aozora00321@gmail.com>
2023-10-17 15:58:26 +0800

    update sidebars

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -4,30 +4,34 @@ import { Icon } from "@iconify/vue";
 // 精选图标：https://theme-hope.vuejs.press/zh/guide/interface/icon.html#iconfont-%E7%B2%BE%E9%80%89%E5%9B%BE%E6%A0%87
 export default navbar([
   { text: "主页", icon: "home", link: "/" },
-  {
-    text: "工具",
-    icon: "tool",
-    prefix: "/08Tools/",
-    children: [
-      { text: "系统", link: "01系统/01系统.html" },
-      { text: "展示", link: "03展示/03展示.html" },
-      { text: "文件相关", link: "04文件相关/04文件相关.html" },
-      { text: "网页展示", link: "05网页展示/05网页展示.html" },
-    ],
-  },
+
   {
     text: "阅读",
     icon: "Read",
     prefix: "/02Reading/",
-    children: [{ text: "视觉小说", link: "01视觉小说/01视觉小说.html" }],
+    children: [
+      { text: "视觉小说", link: "02 视觉小说/02 视觉小说.html" },
+      { text: "游戏", link: "03 游戏/03 游戏.html" },
+    ],
   },
   {
     text: "Coding",
     icon: "network",
-    prefix: "/04Coding/",
+    prefix: "/04 Coding & Tech/",
     children: [
-      { text: "Git", link: "01Git/01Git.html" },
-      { text: "python", icon: "python", link: "02Python/02Python.html" },
+      {
+        text: "Programming Language",
+        link: "01 Programming Language/01 Programming Language.html",
+      },
+      {
+        text: "Tools & Services",
+        icon: "python",
+        link: "03 Tools & Services/03 Tools & Services.html",
+      },
+      {
+        text: "Coding Ability",
+        link: "04 Coding Ability/04 Coding Ability.html",
+      },
     ],
   },
   // {
