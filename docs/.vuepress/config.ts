cad17b21850b27288106ca4ac777b3cccd4dbea3
TRoYals <aozora00321@gmail.com>
2024-04-15 02:14:53 +0800

    add search function

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,4 +1,3 @@
-import { defaultTheme } from "@vuepress/theme-default";
 import { defineUserConfig } from "vuepress/cli";
 import { viteBundler } from "@vuepress/bundler-vite";
 import theme from "./theme.js";
